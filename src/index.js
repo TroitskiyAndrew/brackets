2d05dfc66f3a0c73c9800828d2e156844bb7d042
TroitskiyAndrew <TroitskiyA@yandex.ru>
2021-09-05 03:27:43 +0500

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let tempStec = [];
+    for (let braket of str.split("")) {
+        if (tempStec.length > 0) {
+          let neededRule = bracketsConfig.find((rul) => {
+              return rul[0] === tempStec[tempStec.length - 1]
+              })
+            if (neededRule && neededRule[1] === braket) {
+                tempStec.pop()
+            } else {
+                tempStec.push(braket);
+            }
+        } else {
+            tempStec.push(braket);
+        }
+    }
+
+    return tempStec.length == 0
 }
